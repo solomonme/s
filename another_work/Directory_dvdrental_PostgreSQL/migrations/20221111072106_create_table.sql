de7e55efd06e47283ff56d46426f236186652e46
Solomon Melaku <57417100+solomonme@users.noreply.github.com>
2023-02-27 17:19:48 +0200

    Update 20221111072106_create_table.sql

--- another_work/Directory_dvdrental_PostgreSQL/migrations/20221111072106_create_table.sql
+++ another_work/Directory_dvdrental_PostgreSQL/migrations/20221111072106_create_table.sql
@@ -1,2 +1,1 @@
--- create "country" table
-CREATE TABLE "public"."country" ("country_id" serial NOT NULL, "country" character varying(50) NOT NULL, "last_update" timestamp NOT NULL DEFAULT now(), PRIMARY KEY ("country_id"));
+
