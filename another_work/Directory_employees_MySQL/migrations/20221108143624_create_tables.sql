5c0eb792f7996ceec02c28a8c3581b105d29f0bc
Solomon Melaku <57417100+solomonme@users.noreply.github.com>
2023-02-27 17:20:55 +0200

    Update 20221108143624_create_tables.sql

--- another_work/Directory_employees_MySQL/migrations/20221108143624_create_tables.sql
+++ another_work/Directory_employees_MySQL/migrations/20221108143624_create_tables.sql
@@ -1,2 +1,1 @@
--- create "current_dept_emp" table
-CREATE TABLE `current_dept_emp` (`emp_no` int NOT NULL, `dept_no` char(4) NOT NULL, `from_date` date NULL, `to_date` date NULL) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT "VIEW";
+
